1cbc68fa55f0f015e0fc3d2bf9bd0b7c3835d0fc
helmelig <hisham.elmeligi@valeo.com>
2017-09-28 15:20:08 +0200

    Add arduino serial test sketch

---
+++ Arduino-Serial-Sketch/run_serial_test/run_serial_test.ino
@@ -0,0 +1,15 @@
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Application Started");  
+}
+
+
+void loop() {
+  Serial.println("Hello From The Other Side!");
+  delay(1000);
+}
+
+
+
+
+
